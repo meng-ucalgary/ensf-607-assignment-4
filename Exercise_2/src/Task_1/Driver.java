eced6fd5b1ea328aa6716da63326d136951e9661
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-31 02:34:49 -0600

    task1: implemented driver and randomized

---
+++ Exercise_2/src/Task_1/Driver.java
@@ -0,0 +1,41 @@
+package Task_1;
+
+public class Driver {
+    static final int n = 5;
+
+    public static void main(String[] args) {
+        // create n number of objects of Randomized
+        Randomized[] randomized_list = new Randomized[Driver.n];
+
+        for (int i = 0; i < randomized_list.length; i++) {
+            randomized_list[i] = new Randomized();
+        }
+
+        // create n number of threads
+        Thread[] thread_list = new Thread[Driver.n];
+
+        for (int i = 0; i < thread_list.length; i++) {
+            thread_list[i] = new Thread(randomized_list[i]);
+            thread_list[i].start();
+        }
+
+        try {
+            // wait to complete all threads
+            for (int i = 0; i < thread_list.length; i++) {
+                thread_list[i].join();
+            }
+        }
+
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < randomized_list.length; i++) {
+            sum += randomized_list[i].getGeneratedNumber();
+        }
+
+        System.out.printf("%nSum of generated numbers = %d%n", sum);
+    }
+}
