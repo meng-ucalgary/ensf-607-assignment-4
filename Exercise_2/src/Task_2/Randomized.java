11ce6431c2369b7f459c66da5d9efd5a2e99b700
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-31 02:35:23 -0600

    task2: switched to common arraylist instead of individual storing

---
+++ Exercise_2/src/Task_2/Randomized.java
@@ -0,0 +1,32 @@
+package Task_2;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Randomized implements Runnable {
+    static private ArrayList<Integer> generatedNumbers = new ArrayList<>();
+
+    @Override
+    public void run() {
+        Random r = new Random();
+
+        int generatedNumber = r.nextInt(100) + 1;
+        Randomized.addGeneratedNumbers(generatedNumber);
+
+        System.out.printf("Generated randomized number = %3s%n", String.format("%d", generatedNumber));
+    }
+
+    public static synchronized void addGeneratedNumbers(int num) {
+        Randomized.generatedNumbers.add(num);
+    }
+
+    public static int getSum() {
+        int sum = 0;
+
+        for (int i = 0; i < Randomized.generatedNumbers.size(); i++) {
+            sum += Randomized.generatedNumbers.get(i);
+        }
+
+        return sum;
+    }
+}
